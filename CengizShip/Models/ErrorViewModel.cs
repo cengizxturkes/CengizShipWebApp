d5027cdffe245d432f0d7ae6c4f42a8bfa16a30c
Cengiz <turkes2214@gmail.com>
2023-04-07 07:39:26 +0300

    Add project files.

---
+++ CengizShip/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CengizShip.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
